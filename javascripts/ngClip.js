437e6e0a2e07d23ec6801bdef11a407c3cbfb82a
Asaf David <asafdav@gmail.com>
2013-11-14 02:19:36 +0200

    Added ngClip

---
+++ javascripts/ngClip.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('ngClipboard', []).
+  value('ZeroClipboardPath', '//cdnjs.cloudflare.com/ajax/libs/zeroclipboard/1.1.7/ZeroClipboard.swf').
+  directive('clipCopy', ['$window', 'ZeroClipboardPath', function ($window, ZeroClipboardPath) {
+    return {
+      scope: {
+        clipCopy: '&',
+        clipClick: '&'
+      },
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        // Create the clip object
+        var clip = new ZeroClipboard( element, {
+          moviePath: ZeroClipboardPath
+        });
+
+        clip.on( 'mousedown', function(client) {
+          client.setText(scope.$eval(scope.clipCopy));
+          if (angular.isDefined(attrs.clipClick)) {
+            scope.$apply(scope.clipClick);
+          }
+        });
+      }
+    }
+  }]);
