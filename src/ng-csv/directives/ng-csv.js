61c94bc9fb84f5b6841808f01d7e2e7f4c113805
Darius <darius.aliulis@gmail.com>
2014-01-09 13:04:29 +0200

    minor refactoring

--- src/ng-csv/directives/ng-csv.js
+++ src/ng-csv/directives/ng-csv.js
@@ -5,67 +5,71 @@
  * Author: asafdav - https://github.com/asafdav
  */
 angular.module('ngCsv.directives', []).
-  directive('ngCsv', ['$parse', function($parse) {
+  directive('ngCsv', ['$parse', function ($parse) {
     return {
       restrict: 'AC',
       replace: true,
       transclude: true,
-      scope: { data:'&ngCsv', filename:'@filename', header: '&csvHeader', txtDelim: '@textDelimiter', fieldSep: '@fieldSeparator'},
-      controller: ['$scope', '$element', '$attrs', '$transclude', function($scope, $element, $attrs, $transclude) {
-        $scope.csv = "";
-        $scope.$watch($scope.data, function(newValue, oldValue) {
+      scope: {
+        data:'&ngCsv',
+        filename:'@filename',
+        header: '&csvHeader',
+        txtDelim: '@textDelimiter',
+        fieldSep: '@fieldSeparator'
+      },
+      controller: ['$scope', '$element', '$attrs', '$transclude', function (
+          $scope, $element, $attrs, $transclude
+      ) {
+        $scope.csv = '';
+        $scope.$watch($scope.data, function (newValue) {
           $scope.buildCsv(newValue);
         }, true);
 
-        $scope.buildCsv = function(data) {
-          var csvContent = "data:text/csv;charset=utf-8,";
+        $scope.buildCsv = function (data) {
+          var csvContent = 'data:text/csv;charset=utf-8,';
 
           // Check if there's a provided header array
-          if (angular.isDefined($attrs.csvHeader)) {
-            var header = $scope.$eval($scope.header);
-            var encodingArray, headerString;
-            if (angular.isArray(header)) {
-              encodingArray = header;
-            } else {
-              encodingArray = [];
-              angular.forEach(header, function(title, key){
-                this.push(title);
-              }, encodingArray);
-            }
+          var header = $scope.header();
+          if (header) {
+            var encodingArray = [];
+            angular.forEach(header, function (title) {
+              this.push(title);
+            }, encodingArray);
 
-            headerString = encodingArray.join(",");
-            csvContent += headerString + "\n";
+            var headerString = encodingArray.join($scope.fieldSep || ',');
+            csvContent += headerString + '\n';
           }
 
           // Process the data
-          angular.forEach(data, function(row, index){
+          angular.forEach(data, function (row, index) {
             var infoArray = [];
-            angular.forEach(row, function(field){
+            angular.forEach(row, function (field) {
               if (typeof field === 'string' && $scope.txtDelim) {
                 field = $scope.txtDelim + field + $scope.txtDelim;
               }
               this.push(field);
             }, infoArray);
-            dataString = infoArray.join($scope.fieldSep || ",");
-            csvContent += index < data.length ? dataString + "\n" : dataString;
+            dataString = infoArray.join($scope.fieldSep || ',');
+            csvContent += index < data.length ? dataString + '\n' : dataString;
           });
 
           $scope.csv = encodeURI(csvContent);
         };
 
-        $scope.getFilename = function() {
-          return $scope.filename ? $scope.filename : "download.csv";
+        $scope.getFilename = function () {
+          return $scope.filename ? $scope.filename : 'download.csv';
         };
       }],
       template: '<div class="csv-wrap">' +
         '<div class="element" ng-transclude></div>' +
-        '<a class="hidden-link" ng-hide="true" ng-href="{{ csv }}" download="{{ getFilename() }}"></a>' +
-      '</div>',
-      link: function(scope, element, attrs) {
+        '<a class="hidden-link" ng-hide="true" ng-href="{{ csv }}"' +
+        ' download="{{ getFilename() }}"></a>' +
+        '</div>',
+      link: function (scope, element, attrs) {
         var subject = angular.element(element.children()[0]),
             link = angular.element(element.children()[1]);
 
-        subject.bind('click', function(e) {
+        subject.bind('click', function (e) {
           link[0].click();
         });
       }
