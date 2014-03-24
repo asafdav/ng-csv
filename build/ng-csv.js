5ee829ba7acb7bd6848aa5d9d2a71336b3494ad2
Dotan Simha <dotansimha@gmail.com>
2014-03-24 20:41:13 +0200

    fix click, added ability for lazy load, added ability to use "header" as function for dynamiclly create it,

--- build/ng-csv.js
+++ build/ng-csv.js
@@ -32,7 +32,7 @@ angular.module('ngCsv',
  * Author: asafdav - https://github.com/asafdav
  */
 angular.module('ngCsv.directives', []).
-  directive('ngCsv', ['$parse', function ($parse) {
+  directive('ngCsv', ['$parse', '$q', function ($parse, $q) {
     return {
       restrict: 'AC',
       replace: true,
@@ -43,6 +43,7 @@ angular.module('ngCsv.directives', []).
         header: '&csvHeader',
         txtDelim: '@textDelimiter',
         fieldSep: '@fieldSeparator',
+        lazyLoad: '@lazyLoad',
         ngClick: '&'
       },
       controller: [
@@ -67,61 +68,112 @@ angular.module('ngCsv.directives', []).
 
           $scope.csv = '';
 
-          $scope.$watch(function (newValue) {
-            $scope.buildCsv();
-          }, true);
+          if (angular.isDefined($scope.lazyLoad) && $scope.lazyLoad == "true")
+          {
+            $scope.$watch(function (newValue) {
+              $scope.buildCsv($scope.data(), function() { } );
+            }, true);
+          }
 
-          $scope.buildCsv = function () {
-            var data = $scope.data();
-            var csvContent = 'data:text/csv;charset=utf-8,';
+          $scope.buildCsv = function (data, callback) 
+          {
+            var csvContent = "data:text/csv;charset=utf-8,";
 
-            // Check if there's a provided header array
-            var header = $scope.header();
-            if (header) {
-              var encodingArray = [];
-              angular.forEach(header, function (title) {
-                this.push(title);
-              }, encodingArray);
+            var deferred = $q.defer();
+            var promise = deferred.promise;
 
-              var headerString = encodingArray.join($scope.fieldSep || ',');
-              csvContent += headerString + '\r\n';
+            promise.then(function () 
+            {
+              // Check if there's a provided header array
+              if (angular.isDefined($attrs.csvHeader)) 
+              {
+                var header = $scope.$eval($scope.header);
+                var encodingArray, headerString;
+
+                if (angular.isArray(header)) 
+                {
+                  encodingArray = header;
+                } 
+                else 
+                {
+                  encodingArray = [];
+                  angular.forEach(header, function(title, key)
+                  {
+                    this.push(title);
+                  }, encodingArray);
+                }
+
+                headerString = encodingArray.join(",");
+                csvContent += headerString + "\n";
+              }
+
+              var arrData = data();
+
+              angular.forEach(arrData, function(row, index)
+              {
+                var dataString, infoArray;
+
+                if (angular.isArray(row)) 
+                {
+                  infoArray = row;
+                } 
+                else 
+                {
+                  infoArray = [];
+
+                  angular.forEach(row, function(field, key)
+                  {
+                    this.push(field);
+                  }, infoArray);
+                }
+
+                dataString = infoArray.join(",");
+                csvContent += index < arrData.length ? dataString + "\n" : dataString;
+              });
+
+              $scope.csv = encodeURI(csvContent);
+
+              }).then(function () 
+              {
+                callback();
+              });
+
+              deferred.resolve();
             }
 
-            // Process the data
-            angular.forEach(data, function (row, index) {
-              var infoArray = [];
-              angular.forEach(row, function (field) {
-                this.push(stringifyCell(field));
-              }, infoArray);
-              dataString = infoArray.join($scope.fieldSep || ',');
-              csvContent += index < data.length ? dataString + '\r\n' : dataString;
-            });
-
-            $scope.csv = encodeURI(csvContent);
-            return $scope.csv;
-          };
-
-          $scope.getFilename = function () {
-            return $scope.filename || 'download.csv';
-          };
+            $scope.getFilename = function () 
+            {
+              return $scope.filename || 'download.csv';
+            }
         }
       ],
       template: '<div class="csv-wrap">' +
         '<div class="element" ng-transclude></div>' +
-        '<a class="hidden-link" ng-hide="true" ng-href="{{ csv }}"' +
-        ' download="{{ getFilename() }}"></a>' +
+        '<a class="hidden-link" ng-hide="true" ng-href="{{ csv }}" download="{{ getFilename() }}"></a>' +
         '</div>',
       link: function (scope, element, attrs) {
         var subject = angular.element(element.children()[0]),
             link = angular.element(element.children()[1]);
 
-        subject.bind('click', function (e) {
+        function doClick()
+        {
+          link[0].href = "";
           link[0].click();
+          link[0].href = scope.csv;
+          link[0].click();
+        }
+
+        subject.bind('click', function (e) 
+        {
+          scope.buildCsv(scope.data(), function(){
+            doClick();
+          });
+
           if (!!scope.ngClick) {
             scope.ngClick();
           }
         });
       }
-    };
+    }
   }]);
 })(window, document);
