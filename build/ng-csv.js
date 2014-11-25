45b801dd7f5dbd83d6641e12beea500fbcd9dead
Asaf David <asafdav@gmail.com>
2014-11-26 00:18:10 +0200

    New build

--- build/ng-csv.js
+++ build/ng-csv.js
@@ -216,7 +216,7 @@ angular.module('ngCsv.directives').
       link: function (scope, element, attrs) {
         function doClick() {
           if(window.navigator.msSaveOrOpenBlob) {
-            var blob = new Blob([scope.csv],{
+            var blob = new Blob([decodeURIComponent(scope.csv)],{
                     type: "text/csv;charset=utf-8;"
                 });
             navigator.msSaveBlob(blob, scope.getFilename());
